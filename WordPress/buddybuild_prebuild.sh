3f0aa19a8671b08050acb23d1e802a12bd40f829
Sendhil Panchadsaram <sendhilp@gmail.com>
2016-08-08 14:49:23 -0700

    Updating buddybuild_prebuild.sh to copy the secure files to the home dir.

--- WordPress/buddybuild_prebuild.sh
+++ WordPress/buddybuild_prebuild.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 echo "warning: Detected Prebuild Step"
+cp ${BUDDYBUILD_SECURE_FILES}/wpcom_app_credentials ~/.wpcom_app_credentials
+cp ${BUDDYBUILD_SECURE_FILES}/wpcom_internal_app_credentials ~/.wpcom_internal_app_credentials
