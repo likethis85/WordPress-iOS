d4cfeca4ee0d2289508dfc81224f9e74c65aa2a9
Sendhil Panchadsaram <sendhilp@gmail.com>
2016-08-08 16:38:31 -0700

    Tweaking buddybuild_prebuild.sh

--- WordPress/buddybuild_prebuild.sh
+++ WordPress/buddybuild_prebuild.sh
@@ -3,4 +3,5 @@
 echo "warning: Detected Prebuild Step"
 cp ${BUDDYBUILD_SECURE_FILES}/wpcom_app_credentials ~/.wpcom_app_credentials
 cp ${BUDDYBUILD_SECURE_FILES}/wpcom_internal_app_credentials ~/.wpcom_internal_app_credentials
+cp ${BUDDYBUILD_SECURE_FILES}/wpcom_alpha_app_credentials ~/.wpcom_alpha_app_credentials
 echo "warning: Copied files over"
