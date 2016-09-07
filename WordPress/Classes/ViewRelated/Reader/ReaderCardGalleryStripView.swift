22b2b283a331de70936ca1cc0043dd377148d775
Matt Bumgardner <matt@bumgardner.org>
2016-09-07 10:59:00 -0500

    Roughed in gallery pics.

--- WordPress/Classes/ViewRelated/Reader/ReaderCardGalleryStripView.swift
+++ WordPress/Classes/ViewRelated/Reader/ReaderCardGalleryStripView.swift
@@ -33,8 +33,49 @@ import WordPressShared
     }
 
     private func configureGalleryImageStrip(contentProvider: ReaderPostContentProvider) {
-        //TODO: Stuff goes here
+        // Always clear the previous images so there are no stale or unexpected image
+        // momentarily visible.
+        reset()
 
+        let size = CGSize(width:self.galleryImageView1.frame.width, height:self.galleryImageView1.frame.height)
+        for item in contentProvider.galleryImages() {
+
+            let galleryImageURL = NSURL(string: item as! String)
+            if !(contentProvider.isPrivate()) {
+                let url = PhotonImageURLHelper.photonURLWithSize(size, forImageURL: galleryImageURL)
+                galleryImageView1.setImageWithURL(url, placeholderImage:nil)
+
+            } else if (galleryImageURL!.host != nil) && galleryImageURL!.host!.hasSuffix("wordpress.com") {
+                // private wpcom image needs special handling.
+                let url = WPImageURLHelper.imageURLWithSize(size, forImageURL: galleryImageURL!)
+                let request = requestForURL(url)
+                galleryImageView1.setImageWithURLRequest(request, placeholderImage: nil, success: nil, failure: nil)
+
+            } else {
+                // private but not a wpcom hosted image
+                galleryImageView1.setImageWithURL(galleryImageURL!, placeholderImage:nil)
+            }
+        }
     }
 
+    private func requestForURL(url:NSURL) -> NSURLRequest {
+        var requestURL = url
+
+        let absoluteString = requestURL.absoluteString
+        if !(absoluteString.hasPrefix("https")) {
+            let sslURL = absoluteString.stringByReplacingOccurrencesOfString("http", withString: "https")
+            requestURL = NSURL(string: sslURL)!
+        }
+
+        let request = NSMutableURLRequest(URL: requestURL)
+
+        let acctServ = AccountService(managedObjectContext: ContextManager.sharedInstance().mainContext)
+        if let account = acctServ.defaultWordPressComAccount() {
+            let token = account.authToken
+            let headerValue = String(format: "Bearer %@", token)
+            request.addValue(headerValue, forHTTPHeaderField: "Authorization")
+        }
+
+        return request
+    }
 }
